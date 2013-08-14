ba79b226c405342e38ee8fc03e081872e5f8f0c3
Pratik Bothra <pratik@pratik-Vostro-3560.(none)>
2013-08-14 14:16:29 +0530

    Correcting migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813131325) do
+ActiveRecord::Schema.define(version: 20130814084112) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,7 +28,8 @@ ActiveRecord::Schema.define(version: 20130813131325) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
